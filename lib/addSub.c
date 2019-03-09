5a7fe3d89709cc80a7d52ae9f0965ec917bddae7
Madhu <48263722+Madhuputul15@users.noreply.github.com>
2019-03-09 17:50:50 +0530

    Add files via upload

---
+++ lib/addSub.c
@@ -0,0 +1,11 @@
+# include <header.h>
+
+int add(int num1, int num2)
+{
+	return (num1+num2);
+}
+
+int sub(int num1, int num2)
+{
+	return (num1-num2);
+}
---
+++ lib/mulDiv.c
@@ -0,0 +1,11 @@
+# include <header.h>
+
+int mul(int num1, int num2)
+{
+	return (num1*num2);
+}
+
+int div(int num1, int num2)
+{
+	return (num1/num2);
+}
